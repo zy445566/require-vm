238748fcfaa26d9488b90096b8e302f6815bce52
zy445566 <zyct@vip.qq.com>
2020-12-30 11:52:17 +0800

    feature:add RequireVmModule

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,9 @@
 export function requireVm(reqest:string,contextObject:Object,options:Object,moduleMap:Object,isCache:boolean):any;
 export default function requireVm(reqest:string,contextObject:Object,options:Object,moduleMap:Object,isCache:boolean):any;
+export class RequireVmModule {
+  constructor(filename:string, contextObject:Object, options:Object, moduleMap:Object, isCache:boolean);
+
+  createRequireVm(filename:string):(reqest:string)=>any;
+
+  requireVm(reqest:string):any;
+}
--- index.js
+++ index.js
@@ -2,16 +2,85 @@ const vm = require('vm');
 const fs = require('fs');
 const path = require('path');
 
-function createRequireVm(filename, options={}, moduleMap={}, isCache=false) {
-  return (reqest, contextObject={}) => {
-    options.paths = [path.dirname(filename)];
-    return requireVm(reqest, contextObject, options, moduleMap, isCache);
-  };
+function getResolvePath(reqest, paths) {
+  const jsPath = require.resolve(reqest, {paths});
+  return jsPath;
 }
-function requireVm(reqest, contextObject={}, options={}, moduleMap={}, isCache=false) {
-  if(moduleMap[reqest]) {
-    return moduleMap[reqest];
+class RequireVmModule {
+  constructor(filename, contextObject= {}, options={}, moduleMap={}, isCache=false) {
+    this.filename = filename;
+    this.contextObject = contextObject;
+    this.options = options;
+    this.moduleMap = moduleMap;
+    this.isCache = isCache;
+    this._cacheMap = {};
   }
+
+  createRequireVm(filename) {
+    const requireVmModuleInstance = new RequireVmModule(filename, this.contextObject, Object.assign({
+      ...this.options,
+    }, {paths:[path.dirname(filename)]}), this.moduleMap, this.isCache);
+    return function(reqest){
+      return requireVmModuleInstance.requireVm(reqest);
+    };
+  }
+
+  requireVm(reqest) {
+    if(this.moduleMap[reqest]) {
+      return this.moduleMap[reqest];
+    }
+    const jsPath = getResolvePath(reqest, this.options.paths);
+    if(!path.isAbsolute(jsPath)) {
+      throw new Error(`can't find module<${jsPath}>,you maybe need moduleMap params`);
+    }
+    let jsCode = 'var require;' + 
+      'if(module.createRequire){'+
+        'require = module.createRequire(__filename)'+
+      '};';
+    let codeScript;
+    if(this.isCache && this._cacheMap[jsPath]){
+      jsCode += this._cacheMap[jsPath].code;
+      codeScript = this._cacheMap[jsPath].script;
+      this.options.cachedData = this._cacheMap[jsPath].cachedData;
+    } else {
+      if(!fs.existsSync(jsPath)) {
+        throw new Error(`can't find file:${jsPath}`);
+      }
+      jsCode += fs.readFileSync(jsPath);
+      codeScript = new vm.Script(jsCode, this.options);
+      if(this.isCache && codeScript.createCachedData) {
+        this._cacheMap[jsPath] = {
+          code:jsCode,
+          script:codeScript,
+          cachedData:codeScript.createCachedData(),
+        };
+      }
+    }
+    if(!this.options.filename) {
+      this.options.filename = jsPath;
+    }
+    if(!this.contextObject.__dirname) {
+      this.contextObject.__dirname = path.dirname(jsPath);
+    }
+    if(!this.contextObject.__filename) {
+      this.contextObject.__filename = jsPath;
+    }
+    if(!this.contextObject.module) {
+      this.contextObject.module = {};
+    }
+    if(!this.contextObject.module.exports) {
+      this.contextObject.module.exports = {};
+    }
+    if(!this.contextObject.module.createRequire) {
+      this.contextObject.module.createRequire = this.createRequireVm;
+    }
+    this.contextObject.exports = this.contextObject.module.exports;
+    codeScript.runInNewContext(this.contextObject, this.options);
+    return this.contextObject.module.exports;
+  }
+}
+
+function requireVm(reqest, contextObject={}, options={}, moduleMap={}, isCache=false) {
   if(!options.paths) {
     const resolvePath = [];
     if(module.parent && module.parent.filename) {
@@ -19,59 +88,11 @@ function requireVm(reqest, contextObject={}, options={}, moduleMap={}, isCache=f
     }
     options.paths = resolvePath;
   }
-  const jsPath = require.resolve(reqest, {paths:options.paths});
-  if(!path.isAbsolute(jsPath)) {
-    throw new Error(`can't find module<${jsPath}>,you maybe need moduleMap params`);
-  }
-  let jsCode = 'var require;' + 
-    'if(module.createRequire){'+
-      'require = module.createRequire(__filename)'+
-    '};';
-  let codeScript;
-  if(isCache && requireVm.cacheMap[jsPath]){
-    jsCode += requireVm.cacheMap[jsPath].code;
-    codeScript = requireVm.cacheMap[jsPath].script;
-    options.cachedData = requireVm.cacheMap[jsPath].cachedData;
-  } else {
-    if(!fs.existsSync(jsPath)) {
-      throw new Error(`can't find file:${jsPath}`);
-    }
-    jsCode += fs.readFileSync(jsPath);
-    codeScript = new vm.Script(jsCode, options);
-    if(isCache && codeScript.createCachedData) {
-      requireVm.cacheMap[jsPath] = {
-        code:jsCode,
-        script:codeScript,
-        cachedData:codeScript.createCachedData(),
-      };
-    }
-  }
-  if(!options.filename) {
-    options.filename = jsPath;
-  }
-  if(!contextObject.__dirname) {
-    contextObject.__dirname = path.dirname(jsPath);
-  }
-  if(!contextObject.__filename) {
-    contextObject.__filename = jsPath;
-  }
-  if(!contextObject.module) {
-    contextObject.module = {};
-  }
-  if(!contextObject.module.exports) {
-    contextObject.module.exports = {};
-  }
-  if(!contextObject.module.createRequire) {
-    contextObject.module.createRequire = createRequireVm;
-  }
-  contextObject.exports = contextObject.module.exports;
-  codeScript.runInNewContext(contextObject, options);
-  return contextObject.module.exports;
+  const filename = getResolvePath(reqest, options.paths);
+  const requireVmModuleInstance = new RequireVmModule(filename, contextObject, options, moduleMap, isCache);
+  return requireVmModuleInstance.requireVm(reqest);
 }
-
-requireVm.cacheMap = {};
-
 module.exports = requireVm;
 module.exports.default = requireVm;
 module.exports.requireVm = requireVm;
-module.exports.createRequireVm = createRequireVm;
+module.exports.RequireVmModule = RequireVmModule;
